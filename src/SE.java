02776a67e919f8bb77dd2ab62fdfe1a3fbea08bc
matvienko <i.matvienko@I-MATVIENKO.tscrm.com>
2013-01-30 17:04:18 +0200

    repeater created

--- src/SE.java
+++ src/SE.java
@@ -2,8 +2,17 @@
 public class SE {
 
 	private static int level = 0; 
-	//private static String delim = "--";
-	private static String nodeDelim = nodeDelim.repeat("--", level);
+	private static String delim = "--";
+	private static String nodeDelim = repeat(delim, level);
+	
+	public static String repeat(String str, int times){
+		String res = "";
+		res = str;	
+			for (int i = 0; i < times; i++) {
+				res = res + res;
+			}
+		return res;
+	}
 	
 	public static void showTree(Node nodeRoot){
 		SE.level++;
@@ -16,10 +25,10 @@ public class SE {
 	
 	public static void showItems(Node node){
 		if (node.getItems().isEmpty() == true) {
-			System.out.println("|" +  + " !no items inside this node");
+			System.out.println("|" + SE.nodeDelim  + " !no items inside this node");
 		} else {
 			for (Item item : node.getItems()) {
-				System.out.println("|--! " + item.getValue());
+				System.out.println("|" + SE.nodeDelim + "! " + item.getValue());
 			}
 		}
 	}
